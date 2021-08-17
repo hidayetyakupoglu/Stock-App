aeb74a3673bef0480afa9db0125ea6ab6434ccf7
crodriguezm2016 <crodriguezm2016@gmail.com>
2021-08-16 22:25:50 -0700

    updates to time choices!

--- stockapp.py
+++ stockapp.py
@@ -14,21 +14,34 @@ Use the slider to change the amount of **months** back from today that will be p
 
 """)
 
+timeChoices = {'Daily':['Days',365,'1d'],'Weekly':['Weeks',104,'1w'],'Monthly':['Months',60,'1m'],'Quarterly':['Quarters',48,'3m']}
+timeDf = pd.DataFrame(timeChoices)
+
 # This takes input from the user on which stock ticker that they would like to look at and gives a sliding scale to choose how much data to look back at in months
 tickerSymbol = st.text_input("Enter a ticker symbol: ", "TSLA")
-monthsSlider = st.slider("Number of Previous Months", min_value=1, max_value = 60, value = 12, step = 1)
+timeChoiceSlider = st.select_slider("Choose the type of data you would like to pull: ", options=["Daily", "Weekly", "Montly", "Quarterly"])
+timeSlider = st.slider("Number of Previous %s" % timeDf[timeChoiceSlider][0], min_value=1, max_value = timeDf[timeChoiceSlider][1], value = timeDf[timeChoiceSlider][0] / 2, step = 1)
 
 #Input ticker symbol sent to yfinance to get additional data long name of the company is pulled from the ticker
 tickerData = yf.Ticker(tickerSymbol)
 
 companyName = tickerData.info['longName']
 
+if timeChoiceSlider == "Daily":
+    timeAmount = timeSlider
+elif timeChoiceSlider == "Weekly":
+    timeAmount = timeSlider * 7
+elif timeChoiceSlider == "Monthly":
+    timeAmount = timeSlider * 30
+else:
+    timeAmount = timeSlider * 90
+
 #Pulling date from today for most updated info, and picks a starting date based on slider
 endDay = date.today()
-startDay = endDay - relativedelta(months=monthsSlider)
+startDay = endDay - relativedelta(day=timeAmount)
 
 #history data values pulled based on startDay, endDay and the ticker previously chosen.
-tickerDf = tickerData.history(period = '1d', start=startDay, end=endDay)
+tickerDf = tickerData.history(period = '1d', start=startDay, end=endDay, interval=timeDf[timeChoiceSlider][2])
 
 st.write("""
 Shown are the stock opening, closing, high,low, and volume on the day for %s.
